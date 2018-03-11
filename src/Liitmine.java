f7962c2e962b5b544a640dd8df6401184b91e376
Kristiina <kristiina797@gmail.com>
2018-03-11 22:07:13 +0200

    Siin pole veel midagi, ma olen üllatunud, et ma niigi kaugele jõudsin

---
+++ src/Liitmine.java
@@ -0,0 +1,5 @@
+public class Liitmine {
+    private int liidetav1;
+    private int liidetav2;
+    private int summa;
+}
---
+++ src/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Ma ei tea, kuidas me selle asja valmis jõuame");
+    }
+}
