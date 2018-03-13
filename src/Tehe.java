e8ea4ebae3cc2479136ae8b45925e4c776433c88
Veronika <veronika.lehesaar@gmail.com>
2018-03-13 23:00:00 +0200

    Muutsin, et tehted toimuks antud piirides, jagamisega veel ei mõelnud miskit tarka välja

--- src/Tehe.java
+++ src/Tehe.java
@@ -7,21 +7,29 @@ public class Tehe {
 
     public void prindiTehe(){
         arv1 = (int)(Math.random()*vahemik);
-        arv2 = (int)(Math.random()*vahemik);
         String mark;
         if (tehe.equals("liitmine")){
+            arv2 = (int) (Math.random() * (vahemik-arv1));
             mark = " + ";
             tulemus = arv1 + arv2;
         }
         else if (tehe.equals("lahutamine")){
+            arv2 = (int) (Math.random() * arv1);
             mark = " - ";
             tulemus = arv1 - arv2;
         }
         else if (tehe.equals("korrutamine")){
+            if (arv1 == 0){
+                arv2 = (int) (Math.random() *vahemik);
+            }
+            else{
+                arv2 = (int)(Math.round(Math.random()*(vahemik/arv1)));//panin siia math.round sest muidu tekkisid enamus tehteid suur arv * 0
+            }
             mark = " * ";
             tulemus = arv1*arv2;
         }
         else{
+            arv2 = (int)(Math.random()*vahemik);
             mark = " / ";
             int a = arv1;
             this.arv1 = arv1*arv2;
