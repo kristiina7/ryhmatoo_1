fa29965ed82700d232d9c2075c487383a2d2053b
Kristiina <kristiina797@gmail.com>
2018-03-20 23:18:58 +0200

    Jagamisega on korras ja lisasin ka segamini tehete võimaluse

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Test {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Millised tehted? (liitmine/lahutamine/korrutamine/jagamine/kõik):");
+        System.out.println("Millised tehted? (liitmine/lahutamine/korrutamine/jagamine/segamini):");
         String tehe = scan.next();
         System.out.println("Mitme piires: ");
         int vahemik = scan.nextInt();
@@ -12,12 +13,49 @@ public class Test {
         int mitu = scan.nextInt();
 
         ArrayList<Integer> tulemused = new ArrayList<>();
-        for (int i = 0; i < mitu; i++) {
-            Tehe uus = new Tehe(vahemik, tehe);
+
+        for (int i = 0; i < mitu; i++) { //teeb sisestatud arvu tehteid
+            Tehe uus;
+
+            if (tehe.equals("liitmine")){ //vastavalt sellele, mis tehe sisestati
+                uus = new Liitmine(vahemik);
+            }
+            else if(tehe.equals("lahutamine")){
+                uus = new Lahutamine(vahemik);
+            }
+            else if (tehe.equals("korrutamine")){
+                uus = new Korrutamine(vahemik);
+            }
+            else if (tehe.equals("jagamine")){
+                uus = new Jagamine(vahemik);
+            }
+            else if (tehe.equals("segamini")){ //äkki saab ka kuidagi paremini seda kirjutada
+                int arv = (int) (Math.random()*4);
+                switch(arv){
+                    case 0:
+                        uus = new Liitmine(vahemik);
+                        break;
+                    case 1:
+                        uus = new Lahutamine(vahemik);
+                        break;
+                    case 2:
+                        uus = new Korrutamine(vahemik);
+                        break;
+                    default: uus = new Jagamine(vahemik);
+                }
+            }
+
+            else {
+                System.out.println("Vigane sisend");
+                break;
+            }
+
             uus.prindiTehe();
+
             int vastus = scan.nextInt();
             tulemused.add(uus.kontrolli(vastus));
         }
+        System.out.println(tulemused);
 
     }
 }
